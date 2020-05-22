1c5c56ad7738053fedbbdae24c57e5f61ae75e03
Ziyue Li <liziyue241@gmail.com>
2020-05-22 14:38:20 -0400

    ${1}

--- bash_customization-home_general.sh
+++ bash_customization-home_general.sh
@@ -1,15 +1,65 @@
 # TMUX: auto setup tmux session when log in
 export PATH=$PATH:$HOME/.tmux:~/App_build
-# if which tmux >/dev/null 2>&1; then
-# 	#if not inside a tmux session, and if no session is started, start a new session
-# 	test -z "$TMUX" && (tmux attach || tmux-home)
-# fi
+#if which tmux >/dev/null 2>&1; then
+#	#if not inside a tmux session, and if no session is started, start a new session
+#	test -z "$TMUX" && (tmux attach || tmux-home)
+#fi
 
 # CONDA
 # added by Anaconda3 5.1.0 installer
 export PATH="/Applications/anaconda3/bin:$PATH"
 
 alias link_dotfiles="stow -v -R -t ~ ."
+
+# vim mode
+# Use vim keys in tab complete menu:
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
+
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+	       [[ $1 = 'block' ]]; then
+      echo -ne '\e[1 q'
+	    elif [[ ${KEYMAP} == main ]] ||
+			       [[ ${KEYMAP} == viins ]] ||
+				          [[ ${KEYMAP} = '' ]] ||
+						         [[ $1 = 'beam' ]]; then
+		    echo -ne '\e[5 q'
+			  fi
+		  }
+	  zle -N zle-keymap-select
+	  zle-line-init() {
+	      zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+		      echo -ne "\e[5 q"
+		  }
+	  zle -N zle-line-init
+	  echo -ne '\e[5 q' # Use beam shape cursor on startup.
+	  preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+
+# convenient functions
+gcdot () {
+	cwd=$(pwd)
+	cd $DOTFILES_HOME/dotfiles_home
+	git commit -am '${1}'
+	cd $DOTFILES_HOME/dotfiles_tools_li
+	git commit -am '${1}'
+	cd $cwd
+}
+
+ldot () {
+	cwd=$(pwd)
+	cd $DOTFILES_HOME/dotfiles_home
+	stow -v -R -t ~ .
+	cd $DOTFILES_HOME/dotfiles_tools_li
+	stow -v -R -t ~ .
+	cd $cwd
+}
+
 
 add_conda_kernels () {
 	conda activate py3
