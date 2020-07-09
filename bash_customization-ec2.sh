aebf306d4948f90148ae65586793ec7f280c4586
Ziyue Li <liziyue241@gmail.com>
2020-07-09 21:09:35 +0000

    don't automatically launch tmux session

--- bash_customization-ec2.sh
+++ bash_customization-ec2.sh
@@ -1,10 +1,10 @@
 source ~/.bash_prompt
 
 # TMUX: auto setup tmux session when log in
-if which tmux >/dev/null 2>&1; then
-#if not inside a tmux session, and if no session is started, start a new session
-    test -z "$TMUX" && (tmux attach || tmux-ec2)
-fi
+# if which tmux >/dev/null 2>&1; then
+# #if not inside a tmux session, and if no session is started, start a new session
+#     test -z "$TMUX" && (tmux attach || tmux-ec2)
+# fi
 
 
 alias dott='cd ~/Code/dotfiles/dotfiles_tools_li'
